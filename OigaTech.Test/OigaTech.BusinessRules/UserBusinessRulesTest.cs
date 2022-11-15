b54012a1ede7c19096f509161e6be698e19e05ca
Hgaray <41797160+Hgaray@users.noreply.github.com>
2022-11-15 15:20:01 -0500

    Add Unitesting Project Unitest for BR Unit test for Dataccess

---
+++ OigaTech.Test/OigaTech.BusinessRules/UserBusinessRulesTest.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using OigaTech.BusinessRules;
+using OigaTech.DataAccess.Repositories;
+using OigaTech.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OigaTech.Test.OigaTech.BusinessRules
+{
+    public class UserBusinessRulesTest
+    {
+
+        [Fact]
+        public async Task GetAll_GetSeveralUser()
+        {
+            ///Arrange
+            var userRepository = new Mock<IUserRepository>();
+
+            var response = new UserPaginatedResponse
+            {
+                Users = new List<UserDto> {
+                    new UserDto{
+                        FullName="Test1",
+                        UserName="Test1",
+                    },
+                    new UserDto{
+                        FullName="Test1",
+                        UserName="Test1",
+                    }
+                }
+            };
+
+            userRepository.Setup(x => x.GetAll()).ReturnsAsync(response);
+            var userBusinessRules = new UserBusinessRules(userRepository.Object);
+
+            ///Act
+            var result = await userBusinessRules.GetAll();
+
+            //Assert
+            Assert.NotNull(result.Users);
+            Assert.True(result.Users.Any());
+            Assert.Equal(result, response);
+
+        }
+    }
+}
---
+++ OigaTech.Test/OigaTech.DataAccess/UserRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OigaTech.DataAccess;
+using OigaTech.DataAccess.Repositories;
+using OigaTech.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OigaTech.Test.OigaTech.DataAccess
+{
+    public class UserRepositoryTest
+    {
+        [Fact]
+        public async Task Add_NewUser_Successful()
+        {
+            var builder = new DbContextOptionsBuilder<OigaTechDBContext>();
+            builder.UseInMemoryDatabase(databaseName: "OigaTechDB");
+            var dbContextOption = builder.Options;
+
+            var dbContext = new OigaTechDBContext(dbContextOption);
+
+            var newUser = new UserDto
+            {
+                FullName="TestRepo",
+                UserName="userName"
+            };
+
+            var repo = new UserRepository(dbContext);
+
+            var result = await repo.Add(newUser);
+
+            Assert.True(result);
+        }
+    }
+}
